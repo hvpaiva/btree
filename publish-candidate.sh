5ac6caaa6f49d8e5990deaa9a26155351ed6b20b
Highlander Vilela de Paiva Silva <highlander.silva@bancointer.com.br>
2022-08-02 19:15:17 -0300

    creating script for publish

---
+++ publish-candidate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+# assumes cabal 2.4 or later
+cabal v2-sdist
+cabal upload dist-newstyle/sdist/*.tar.gz
+
+cabal v2-haddock --builddir="$dir" --haddock-for-hackage --enable-doc
+cabal upload -d $dir/*-docs.tar.gz
